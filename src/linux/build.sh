d8c2485cee4dab62b3c748f307ce0327fd3896a4
mstop4 <mstop4@gmail.com>
2017-10-20 19:14:42 -0700

    rebuilt linux lib

--- src/linux/build.sh
+++ src/linux/build.sh
@@ -1,5 +1,5 @@
 echo "Building libfmodgms..."
-clang++ -std=c++11 -stdlib=libc++ -m32 -Wall -Wno-unknown-pragmas -fPIC -c fmodgms.cpp
+clang++ -std=c++11 -stdlib=libstdc++ -m32 -Wall -Wno-unknown-pragmas -fPIC -c fmodgms.cpp
 clang++ -m32 -shared -Wl,-soname,libfmodgms.so.0 -o libfmodgms.so fmodgms.o -L. -lfmod
 #ln -sf libfmodgms.so.0.8 libfmodgms.so
 #ln -sf libfmodgms.so.0.8 libfmodgms.so.0
