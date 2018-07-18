888ec85070582a1eec7572043034e2f25c4a3c74
mstop4 <mstop4@gmail.com>
2018-07-18 11:20:15 -0700

    rebuilt .so using 64-bit?

--- src/linux/build.sh
+++ src/linux/build.sh
@@ -1,6 +1,6 @@
 echo "Building libfmodgms..."
-clang++ -std=c++11 -stdlib=libstdc++ -m32 -Wall -Wno-unknown-pragmas -fPIC -c fmodgms.cpp
-clang++ -m32 -shared -Wl,-soname,libfmodgms.so.0 -o libfmodgms.so fmodgms.o -L. -lfmod
-#ln -sf libfmodgms.so.0.9 libfmodgms.so
-#ln -sf libfmodgms.so.0.9 libfmodgms.so.0
+clang++ -std=c++11 -stdlib=libstdc++ -m64 -Wall -Wno-unknown-pragmas -fPIC -c fmodgms.cpp
+clang++ -m64 -shared -Wl,-soname,libfmodgms.so.0 -o libfmodgms.so fmodgms.o -L. -lfmod
+#ln -sf libfmodgms.so.0.10 libfmodgms.so
+#ln -sf libfmodgms.so.0.10 libfmodgms.so.0
 echo "Finished"
