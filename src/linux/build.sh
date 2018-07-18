e88679073b3b526299d6b4c184c952bbc0b89b0d
mstop4 <mstop4@gmail.com>
2018-07-18 12:02:50 -0700

    rebuild

--- src/linux/build.sh
+++ src/linux/build.sh
@@ -1,5 +1,5 @@
 echo "Building libfmodgms..."
-clang++ -std=c++11 -stdlib=libstdc++ -m64 -Wall -Wno-unknown-pragmas -fPIC -c fmodgms.cpp
+clang++ -std=c++14 -stdlib=libstdc++ -m64 -Wall -Wno-unknown-pragmas -fPIC -c fmodgms.cpp
 clang++ -m64 -shared -Wl,-soname,libfmodgms.so.0 -o libfmodgms.so fmodgms.o -L. -lfmod
 #ln -sf libfmodgms.so.0.10 libfmodgms.so
 #ln -sf libfmodgms.so.0.10 libfmodgms.so.0
