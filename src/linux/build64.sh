d284dcfba600d6a5dbc0e6a8e2a750cabfbfb8fc
mstop4 <mstop4@gmail.com>
2018-07-23 16:23:39 -0700

    .so buiild attempt 2

--- src/linux/build64.sh
+++ src/linux/build64.sh
@@ -1,4 +1,4 @@
 echo "Building libfmodgms..."
 clang++ -std=c++14 -stdlib=libc++ -m64 -Wall -Wno-unknown-pragmas -fPIC -c fmodgms.cpp
-clang++ -m64 -shared -Wl,-soname,libfmodgms.so.0 -Wl,-rpath,$ORIGIN -o libfmodgms.so fmodgms.o
+clang++ -m64 -shared -Wl,-soname,libfmodgms.so.0 -Wl,-rpath,$ORIGIN -o libfmodgms.so fmodgms.o -L. -lfmod
 echo "Finished"
