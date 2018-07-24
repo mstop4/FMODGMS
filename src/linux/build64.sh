f79fc8cf7e76f11171ae43809356c6094e4fe50b
mstop4 <mstop4@gmail.com>
2018-07-23 17:24:40 -0700

    .so library build attept 4

--- src/linux/build64.sh
+++ src/linux/build64.sh
@@ -1,4 +1,4 @@
 echo "Building libfmodgms..."
 clang++ -std=c++14 -stdlib=libstdc++ -m64 -Wall -Wno-unknown-pragmas -fPIC -c fmodgms.cpp
-clang++ -m64 -shared -Wl,-soname,libfmodgms.so.0 -Wl,-rpath,. -o libfmodgms.so fmodgms.o -L. -lfmod
+clang++ -m64 -shared -Wl,-soname,libfmodgms.so.0 -Wl,-rpath="${ORIGIN}" -o libfmodgms.so fmodgms.o -L. -lfmod
 echo "Finished"
