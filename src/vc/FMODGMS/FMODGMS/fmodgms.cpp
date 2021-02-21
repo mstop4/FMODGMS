812ec48475d5599384713f504220cde46d2f6630
cam900 <dbtlrchl@naver.com>
2021-02-21 12:38:15 +0900

    Experimental FMOD Core API 2.01.07 update for windows

--- src/vc/FMODGMS/FMODGMS/fmodgms.cpp
+++ src/vc/FMODGMS/FMODGMS/fmodgms.cpp
@@ -3,10 +3,9 @@
 //
 //  FMODGMS v.0.10.0
 //
-//  GML bindings to the FMOD Studio low-level API for
-//  GameMaker:Studio.
+//  GML bindings to the FMOD Core API for GameMaker:Studio.
 //
-//  FMOD Studio version: 1.10.07
+//  FMOD Studio version: 2.01.07
 ----------------------------------------------------------*/
 
 #ifndef FMODGMS_CPP
@@ -498,14 +497,13 @@ GMexport double FMODGMS_Snd_LoadSound_Ext(char* location, double mode, uint64_t*
 		_exInfo.fileuserdata =			(void*)exInfo[24];
 		_exInfo.filebuffersize =		(int)exInfo[25];
 		_exInfo.channelorder =			(FMOD_CHANNELORDER)exInfo[26];
-		_exInfo.channelmask =			(FMOD_CHANNELMASK)exInfo[27];
 		_exInfo.initialsoundgroup =		0; //not supported
-		_exInfo.initialseekposition =	(unsigned int)exInfo[29];
-		_exInfo.initialseekpostype =	(FMOD_TIMEUNIT)exInfo[30];
-		_exInfo.ignoresetfilesystem =	(int)exInfo[31];
-		_exInfo.audioqueuepolicy =		(unsigned int)exInfo[32];
-		_exInfo.minmidigranularity =	(unsigned int)exInfo[33];
-		_exInfo.nonblockthreadid =		(unsigned int)exInfo[34];
+		_exInfo.initialseekposition =	(unsigned int)exInfo[28];
+		_exInfo.initialseekpostype =	(FMOD_TIMEUNIT)exInfo[29];
+		_exInfo.ignoresetfilesystem =	(int)exInfo[30];
+		_exInfo.audioqueuepolicy =		(unsigned int)exInfo[31];
+		_exInfo.minmidigranularity =	(unsigned int)exInfo[32];
+		_exInfo.nonblockthreadid =		(unsigned int)exInfo[33];
 		_exInfo.fsbguid =				0; //not supported
 		result = sys->createSound(location, _mode, &_exInfo, &sound);
 	}
@@ -1873,7 +1871,7 @@ GMexport const char* FMODGMS_Snd_Get_TagStringFromIndex(double soundIndex, doubl
 			FMOD_TAG tag;
 			soundList[si]->getTag(0, ti, &tag);
 
-			if (tag.datatype >= FMOD_TAGDATATYPE_STRING && tag.datatype < FMOD_TAGDATATYPE_CDTOC)
+			if (tag.datatype >= FMOD_TAGDATATYPE_STRING && tag.datatype < FMOD_TAGDATATYPE_MAX)
 			{
 				// 8-bit string
 				if (tag.datatype == FMOD_TAGDATATYPE_STRING || tag.datatype == FMOD_TAGDATATYPE_STRING_UTF8)
@@ -2111,9 +2109,9 @@ GMexport const char* FMODGMS_Snd_Get_TagStringFromName(double soundIndex, char* 
 
 		if (tagFound)
 		{
-			if (tag.datatype >= FMOD_TAGDATATYPE_STRING && tag.datatype < FMOD_TAGDATATYPE_CDTOC)
+			if (tag.datatype >= FMOD_TAGDATATYPE_STRING && tag.datatype < FMOD_TAGDATATYPE_MAX)
 			{
-				if (tag.datatype >= FMOD_TAGDATATYPE_STRING && tag.datatype < FMOD_TAGDATATYPE_CDTOC)
+				if (tag.datatype >= FMOD_TAGDATATYPE_STRING && tag.datatype < FMOD_TAGDATATYPE_MAX)
 				{
 					// 8-bit string
 					if (tag.datatype == FMOD_TAGDATATYPE_STRING || tag.datatype == FMOD_TAGDATATYPE_STRING_UTF8)
